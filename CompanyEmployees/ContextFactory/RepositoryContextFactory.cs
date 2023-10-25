ca8261d62c1a6a40f3f897f0d04297a4b66790f4
Thanos Dimitriades <142520915+thanosngplus@users.noreply.github.com>
2023-10-25 14:04:37 +0300

    add EF entities and Repository Context

---
+++ CompanyEmployees/ContextFactory/RepositoryContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Repository;
+
+namespace CompanyEmployees.ContextFactory
+{
+    // used to create a derived DbContext during design time, for migrations
+    public class RepositoryContextFactory : IDesignTimeDbContextFactory<RepositoryContext>
+    {
+        public RepositoryContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<RepositoryContext>()
+                .UseSqlServer(configuration.GetConnectionString("sqlconnection"));
+
+            return new RepositoryContext(builder.Options);
+        }
+    }
+}
