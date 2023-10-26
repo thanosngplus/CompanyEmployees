1331d6d50ee49b88dc1509098d116d54227a99fa
Thanos Dimitriades <142520915+thanosngplus@users.noreply.github.com>
2023-10-26 17:20:50 +0300

    fix: Mapping Profile constructor param

---
+++ CompanyEmployees/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Entities.Models;
+using Shared.DataTransferObjects;
+
+namespace CompanyEmployees
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Company, CompanyDto>()
+                .ForCtorParam("FullAddress",
+                    opt => opt.MapFrom(company => string.Join(' ', company.Address, company.Country)));
+        }
+    }
+}
